682f960aa49c474207732ca02195e44e6d542fd0
unknown <katharina.r@hotmail.fr>
2020-03-23 16:13:43 +0100

    tableau associatif

---
+++ recettes/recettes.php
@@ -0,0 +1,56 @@
+<?php 
+
+    $entree1 = [
+        "titre" => "Entree 1",
+        "image" => "img/entree1.jpg",
+        "Temps de préparation" => "15sec",
+        "Personnes" => "8",
+        "Difficulté" => "1/4",
+        "Coût" => "1/4",
+        "Ingrédients" => "1 paquet de pâtes de lasagnes",
+        "3 oignons jaunes",
+        "3 gousses d'ail",
+        "branche de céleri", 
+        "carotte",
+        "Etapes" => "Faire revenir gousses hachées d'ail et les oignons émincés dans un peu d'huile d'olive.///
+        Ajouter la carotte et la branche de céleri hachée puis la viande et faire revenir le tout.///
+        Au bout de quelques minutes, ajouter le vin rouge. Laisser cuire jusqu'à évaporation."
+        ];
+
+    $entree2 = [ 
+        "titre" => "Entree 1",
+        "image" => "img/entree1.jpg",
+        "Temps de préparation" => "15sec",
+        "Personnes" => "8",
+        "Difficulté" => "1/4",
+        "Coût" => "1/4",
+        "Ingrédients" => "1 paquet de pâtes de lasagnes",
+        "3 oignons jaunes",
+        "3 gousses d'ail",
+        "branche de céleri", 
+        "carotte",
+        "Etapes" => "Faire revenir gousses hachées d'ail et les oignons émincés dans un peu d'huile d'olive.///
+        Ajouter la carotte et la branche de céleri hachée puis la viande et faire revenir le tout.///
+        Au bout de quelques minutes, ajouter le vin rouge. Laisser cuire jusqu'à évaporation."
+        ];
+
+    $entree3 = [
+        "titre" => "Entree 1",
+        "image" => "img/entree1.jpg",
+        "Temps de préparation" => "15sec",
+        "Personnes" => "8",
+        "Difficulté" => "1/4",
+        "Coût" => "1/4",
+        "Ingrédients" => "1 paquet de pâtes de lasagnes",
+        "3 oignons jaunes",
+        "3 gousses d'ail",
+        "branche de céleri", 
+        "carotte",
+        "Etapes" => "Faire revenir gousses hachées d'ail et les oignons émincés dans un peu d'huile d'olive.///
+        Ajouter la carotte et la branche de céleri hachée puis la viande et faire revenir le tout.///
+        Au bout de quelques minutes, ajouter le vin rouge. Laisser cuire jusqu'à évaporation."
+        ];
+
+    $entrees = [$entree1, $entree2, $entree3];
+
+?>
