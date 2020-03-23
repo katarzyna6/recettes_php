5c058c8a756ef34e6d2578c815ef28d18412e717
unknown <katharina.r@hotmail.fr>
2020-03-23 09:03:42 +0100

    ajout menu en PHP

--- html/menu.php
+++ html/menu.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html lang="fr">
-
-    <head>
-        <title>Mon livre de recettes</title>
-        <meta charset="utf-8">
-         <link rel="stylesheet" type="text/css" href="css/style.css">
-    </head>
-    
-    <body>
-        <header>
-            <nav class="menu">
+<nav class="menu">
                 <ul>
                     <li><a href ="#">Page d'accueil</a></li>
                     <li><a href ="recettes/entree.html">Les entrées</a>
@@ -35,16 +24,3 @@
                     </li>
                 </ul>
             </nav>
-        </header>
-        
-        <article>
-            <h2 class="text-center">Suggestion du jour</h2>
-            <ul class="row items">
-                <li class="img-bullet col-4 text-center"><img src="img/entree3.jpg" alt="Photo de l'entrée 3"><br><a href ="recettes/entrees/entree3.html">Entrée 3</a></li>
-                <li class="img-bullet col-4 text-center"><img src="img/plat1.jpg" alt="Photo du plat 1"><br><a href ="recettes/plats/plat1.html">Plat 1</a></li>
-                <li class="img-bullet col-4 text-center"><img src="img/dessert1.jpg" alt="Photo du dessert 1"><br><a href ="recettes/desserts/dessert1.html">Dessert 1</a></li>
-            </ul>
-        </article>
-    </body>
-
-</html>
---
+++ index.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+
+    <head>
+        <title>Mon livre de recettes</title>
+        <meta charset="utf-8">
+         <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    
+    <body>
+        <header>
+        <?php
+            require "html/menu.php";
+        ?>     
+        </header>
+        
+        <article>
+            <h2 class="text-center">Suggestion du jour</h2>
+            <ul class="row items">
+                <li class="img-bullet col-4 text-center"><img src="img/entree3.jpg" alt="Photo de l'entrée 3"><br><a href ="recettes/entrees/entree3.html">Entrée 3</a></li>
+                <li class="img-bullet col-4 text-center"><img src="img/plat1.jpg" alt="Photo du plat 1"><br><a href ="recettes/plats/plat1.html">Plat 1</a></li>
+                <li class="img-bullet col-4 text-center"><img src="img/dessert1.jpg" alt="Photo du dessert 1"><br><a href ="recettes/desserts/dessert1.html">Dessert 1</a></li>
+            </ul>
+        </article>
+    </body>
+
+</html>
