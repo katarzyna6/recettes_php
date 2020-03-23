a6cc6157e514eb43d200a328ef301b1e09da7c83
unknown <katharina.r@hotmail.fr>
2020-03-23 09:24:18 +0100

    menu + sousmenu en PHP

--- html/menu.php
+++ html/menu.php
@@ -1,25 +1,25 @@
 <nav class="menu">
                 <ul>
                     <li><a href ="#">Page d'accueil</a></li>
-                    <li><a href ="recettes/entree.html">Les entrées</a>
-                        <ul class="sous-menu">
-                            <li><a href ="recettes/entrees/entree1.html">Entrée 1</a></li>
-                            <li><a href ="recettes/entrees/entree2.html">Entrée 2</a></li>
-                            <li><a href ="recettes/entrees/entree3.html">Entrée 3</a></li>
+                    <li><a href ="index.php?page=entree">Les entrées</a>
+                    <ul class="sous-menu">
+                            <li><a href ="index.php?souspage=entree1">Entrée 1</a></li>
+                            <li><a href ="index.php?souspage=entree2">Entrée 2</a></li>
+                            <li><a href ="index.php?souspage=entree3">Entrée 3</a></li>
                         </ul>
                     </li>
-                    <li><a href ="recettes/plat.html">Les plats</a>
+                    <li><a href ="index.php?page=plats">Les plats</a>
                         <ul class="sous-menu">
-                            <li><a href ="recettes/plats/plat1.html">Plat 1</a></li>
-                            <li><a href ="recettes/plats/plat2.html">Plat 2</a></li>
-                            <li><a href ="recettes/plats/plat3.html">Plat 3</a></li>
+                            <li><a href ="index.php?souspage=plat1">Plat 1</a></li>
+                            <li><a href ="index.php?souspage=plat2">Plat 2</a></li>
+                            <li><a href ="index.php?souspage=plat3">Plat 3</a></li>
                         </ul>
                     </li>
-                    <li><a href ="recettes/dessert.html">Les desserts</a>
+                    <li><a href ="index.php?page=desserts">Les desserts</a>
                         <ul class="sous-menu">
-                            <li><a href ="recettes/desserts/dessert1.html">Dessert 1</a></li>
-                            <li><a href ="recettes/desserts/dessert2.html">Dessert 2</a></li>
-                            <li><a href ="recettes/desserts/dessert3.html">Dessert 3</a></li>
+                            <li><a href ="index.php?souspage=dessert1">Dessert 1</a></li>
+                            <li><a href ="index.php?souspage=dessert2">Dessert 2</a></li>
+                            <li><a href ="index.php?souspage=dessert3">Dessert 3</a></li>
                         </ul>
                     </li>
                 </ul>
